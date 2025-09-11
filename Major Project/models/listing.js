65eb9e09e5bf3a85d6ca33a26f9186318a254645
Nikhil Ramteke <ramteken852@gmail.com>
2025-09-11 12:59:36 +0530

    Schema Files Added

--- Major Project/models/listing.js
+++ Major Project/models/listing.js
@@ -7,11 +7,15 @@ const listingSchema = new Schema({
         required: true,
     },
     description: String,
-    image: String,
+    image: {
+        type: String,
+        default: "http://unsplash.com/photos/lush-green-leaves-against-a-bright-blue-sky-S2RXtg6YGt0",
+        set: (v) => v === "" ? "http://unsplash.com/photos/lush-green-leaves-against-a-bright-blue-sky-S2RXtg6YGt0" : v,
+    },
     price: Number,
     location: String,
     country: String,
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
-module.export = Listing;
+module.exports = Listing;
