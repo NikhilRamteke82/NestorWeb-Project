a5ba9e1c1dcd53a6298c40f43212a9b82e9f52ac
Nikhil Ramteke <ramteken852@gmail.com>
2025-09-12 15:59:30 +0530

    Files are updated

--- Major Project/app.js
+++ Major Project/app.js
@@ -34,11 +34,25 @@ app.get("/listings", async(req, res) => {
     res.render("listings/index.ejs", { allListings });
 });
 
+// New Route
+app.get("/listings/new", (req, res) => {
+    res.render("listings/new.ejs")
+});
+
 
 // show route
 app.get("/listings/:id", async(req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", { listing });
+});
+
+// create route
+app.post("/listings", async(req, res) => {
+    // let { title, description, image, price, country, location } = req.body;
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
 });
 
 
